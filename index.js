8092f709aefc45f688ab5ad2f0dfbbc7dd6bfdf0
xunuo <i@xunuo.com>
2015-05-18 20:47:30 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+// Create an app
+var server = require('diet'),
+    request = require('request'),
+    app = server(),
+    config = require('./config.json'), // {"login_token" : "xxxxx,xxxxxxxxxxxxxxx"}
+    loginToken = config.login_token
+    ;
+
+app.listen('http://hateip.com:999');
+
+
+// Listen on GET /
+// example : ?user=snow&ip=127.0.0.1
+app.get('/', function($){
+    
+    var user = $.query.user,
+        ip = $.query.ip
+        ;
+    
+    if(!user || !ip){
+        $.end('{ "code" : 500, "msg" : "no user or ip." }');
+        return false;
+    }
+    
+    request.post(
+        {
+            url:'https://dnsapi.cn/Record.List',
+            form:{
+                domain : 'hateip.com',
+                login_token:loginToken,
+                sub_domain : user,
+                format : 'json'
+            },
+            encoding:'utf8'
+        },
+        function(error, response, body){
+            if(response.statusCode == 200){
+                
+                
+                var callback = JSON.parse(body);
+
+                // 没找到记录
+                if(callback.status.code == '10'){
+                     console.log('新增A记录');
+                     request.post(
+                        {
+                            url:'https://dnsapi.cn/Record.Create',
+                            form:{
+                                domain : 'hateip.com',
+                                login_token:loginToken,
+                                sub_domain : user,
+                                value : ip,
+                                record_type : 'A',
+                                record_line : '默认',
+                                format : 'json'
+                            },
+                            encoding:'utf8'
+                        },
+                        function(error, response, body){
+                            if(response.statusCode == 200){
+                                console.log(body);
+                                $.end('{ "code" : 200, "msg" : "create [' + user + '.hateip.com -> ' + ip + '] success." }');
+                            }else{
+                                console.log(response.statusCode);
+                                $.end('{ "code" : 500, "msg" : "create [' + user + '.hateip.com -> ' + ip + '] success." }');
+                            }
+                        }
+                     );
+                }else{
+                    // 已有重复记录
+                    console.log('已有重复记录,修改记录');
+                    var recordId = callback.records[0].id;
+
+                     request.post(
+                        {
+                            url:'https://dnsapi.cn/Record.Modify',
+                            form:{
+                                domain : 'hateip.com',
+                                login_token:loginToken,
+                                sub_domain : user,
+                                record_id : recordId,
+                                value : ip,
+                                record_type : 'A',
+                                record_line : '默认',
+                                format : 'json'
+                            },
+                            encoding:'utf8'
+                        },
+                        function(error, response, body){
+                            if(response.statusCode == 200){
+                                console.log(body);
+                                $.end('{ "code" : 200, "msg" : "modify [' + user + '.hateip.com -> ' + ip + '] success." }');
+                            }else{
+                                console.log(response.statusCode);
+                                $.end('{ "code" : 500, "msg" : "modify [' + user + '.hateip.com -> ' + ip + '] error." }');
+                            }
+                        }
+                     );
+
+                }
+
+            }else{
+                console.log(response.statusCode);
+                $.end('{ "code" : 500, "msg" : "search domain list error." }');
+            }
+        }
+    );
+    
+    
+})
